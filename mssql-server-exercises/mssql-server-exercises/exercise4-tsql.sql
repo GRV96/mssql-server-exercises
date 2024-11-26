947c61df12b6c12fa345babfd76fb644c5541be3
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-11-25 21:05:27 -0500

    Exercices 4 and 5: tables initialized in the exercises' script

--- mssql-server-exercises/mssql-server-exercises/exercise4-tsql.sql
+++ mssql-server-exercises/mssql-server-exercises/exercise4-tsql.sql
@@ -1,8 +1,34 @@
 CREATE DATABASE EXERCISE4
 GO
 USE EXERCISE4
+GO
 
--- Execute script create-table-contact.sql before the next statements.
+-- Initializing table Contact
+CREATE TABLE Contact(
+lastName VARCHAR(200),
+firstName VARCHAR(200),
+age INT,
+gender CHAR(1),
+birthDate DATE)
+
+INSERT INTO Contact VALUES
+('Marchand', 'Elisabeth', 18, 'F', '1976-08-04'),
+('Truchon', 'Melanie', 16, 'F', '1978-08-04'),
+('Teslu', 'Sandrine', 17, 'F', '1987-05-02'),
+('Portail', 'Bruno', 23, 'M', '1970-05-06'),
+('Virenque', 'Michel', 22, 'M', '1983-08-02'),
+('Dutruel', 'Pascal', 22, 'M', '1983-08-02'),
+('Virenque', 'Micheline', 37, 'F', '1975-08-02'),
+('Fournillet', 'Alain', 22, 'M', '1983-01-01'),
+('Faurnillet', 'Pascal', 48, 'M', '1960-06-12'),
+('Boutin', 'Ludivine', 47, 'F', '1965-01-22'),
+('Delors', 'Valerie', 28, 'F', '1978-09-24'),
+('Thuillier', 'Olivier', 41, 'M', '1976-08-12'),
+('Chuillier', 'Olivier', 41, 'M', '1976-08-12'),
+('Meilhac', 'Amelie', 34, 'F', '1983-05-08'),
+('Boutin', 'Ludivine', 47, 'F', '1965-01-22')
+GO
+-- Table Contact initialized
 
 -- People's age without duplicates
 SELECT DISTINCT age
